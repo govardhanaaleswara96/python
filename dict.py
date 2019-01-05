108a8e8d10cb44e6cd5980aa4050ec61b094a537
govardhanaaleswara96 <govardhanaaleswara88@gmail.com>
2019-01-05 09:46:23 +0530

    added

---
+++ dict.py
@@ -0,0 +1,49 @@
+#dictionary
+# collection of unordered and changeable & indexed 
+a = {
+    "name":"govardhan",
+    "age":"21",
+    "desc":"Software Developer"
+}
+# print(a)
+# x = a["name"]
+# y = a["desc"]
+# print(y)
+# z = a.get("name")
+# a["name"] = "Govardhan Aaleswara"
+# print(a)
+# for x in a:
+#   print(x)  
+# for x in a:
+#     print(a[x]) 
+# for x in a.values():
+#     print(x)
+# for x,y in a.items():
+#     print(x,y)
+# if "name" in a:
+#     print('Yes it is in a')
+# print(len(a))
+# a['dob'] = '2019/01/03'
+# print(a)
+# x = a.pop('name')
+# print(a)
+# a.popitem()
+# print(a)
+# del a["age"]
+# print(a)
+# a.clear()
+# print(a)
+# a = dict(name="govardhan",age="21")
+# print(a)
+# x = a.copy()
+# print(x)
+# b = 0
+# x = dict.fromkeys(a,b)
+# print(x)
+# x = a.keys()
+# print(x)
+# x = a.values()
+# print(x)
+# print(a)
+# x = a.setdefault("name", "GOVARDHAN")
+# print(x)
---
+++ if.py
@@ -0,0 +1,33 @@
+a = 10
+b = 20
+# if a != b:
+#     print('a & b not equal')
+# if a < b:
+#     print('a is less than b')    
+
+# if b > a:
+#     print('b is grater than a')
+
+# if a > b:
+#     print('a is bigger than b')
+# elif a < b:
+#     print('a is less than b')
+
+# if a == b:
+#     print('a and b both are equal')
+# elif a > b:
+#     print(' a is greater than b')
+# else:
+#     print(' b id bigger than a')      
+
+# short hand
+# if a != b: print(' a & b not equal')
+# print(a) if a > b else print(b) 
+
+# print('a is big') if a > b else print('both are equal') if a == b else print('b is big') 
+
+if a < b and b > a:
+    print(' b is bigger than a')
+
+if a == b or b > a:
+    print(' b is bigger than a')    
